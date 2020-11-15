48bd9b9654cf40bcb00034153a3ca4ceacfceb33
Mukul-dhiman <b18076@students.iitmandi.ac.in>
2020-11-15 19:15:20 +0530

    bomber complete setup

--- Assets/scripts/Enemy/bomber/Bomber_chase.cs
+++ Assets/scripts/Enemy/bomber/Bomber_chase.cs
@@ -9,8 +9,9 @@ public class Bomber_chase : MonoBehaviour
     public float speed = 1;
     public float circumference = 5;
     public float count = 0;
-    Vector3 current_center;
     GameObject go;
+    int direction = 1;
+    bool inside = true;
 
     // Update is called once per frame
     void Update()
@@ -28,21 +29,33 @@ public class Bomber_chase : MonoBehaviour
         {
             return;
         }
-        if (Vector3.Distance(transform.position, player.position) > 5)
+        if (inside)
         {
-            Vector3 pos = transform.position;
-            Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
-            pos += transform.rotation * velocity;
-            transform.position = pos;
-            current_center = transform.position-go.transform.position;
+            if (Vector3.Distance(transform.position, player.position) > 4)
+            {
+                Vector3 pos = transform.position;
+                Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+                pos += transform.rotation * velocity;
+                transform.position = pos;
+            }
+            else
+            {
+                inside = false;
+            }
         }
         else
         {
-            count += Time.deltaTime * speed;
-            var x = Mathf.Cos(count) * circumference;
-            var y = Mathf.Sin(count) * circumference;
-            Vector3 targetPosition = new Vector3(x, y, 0);
-            transform.position = targetPosition + current_center;
+            if (Vector3.Distance(transform.position, player.position) < 5)
+            {
+                Vector3 pos = transform.position;
+                Vector3 velocity = new Vector3(-1*maxSpeed * Time.deltaTime,0, 0);
+                pos += transform.rotation * velocity;
+                transform.position = pos;
+            }
+            else
+            {
+                inside = true;
+            }
         }
     }
 }
