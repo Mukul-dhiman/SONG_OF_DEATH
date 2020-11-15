b25ddc79cc5b190fe0b9e262a052950311112c1e
Mukul-dhiman <b18076@students.iitmandi.ac.in>
2020-11-15 21:30:05 +0530

    all set for meteors

--- Assets/scripts/Animation/meteors/meteors_flow.cs
+++ Assets/scripts/Animation/meteors/meteors_flow.cs
@@ -35,99 +35,99 @@ public class meteors_flow : MonoBehaviour
             offset.z = 0;
             offset = offset.normalized * meteor_spawn_distance;
 
-            Vector3 dir = Random.onUnitSphere;
+            Vector3 dir = Random.onUnitSphere*4;
             dir.z = 0;
             dir -= offset;
             int p = Random.Range(1, 101);
             if (p < 15)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg-90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
                 Instantiate(m1, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 30)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m2, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m2, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 40)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m3, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m3, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 50)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m4, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m4, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 60)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m5, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m5, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 67)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m6, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m6, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 74)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m7, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m7, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 80)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m8, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m8, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 85)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m9, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m9, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 88)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m10, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m10, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 91)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m11, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m11, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 94)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m12, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m12, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 96)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m13, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m13, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else if (p < 98)
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m14, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m14, transform.position + offset, Quaternion.Euler(0, 0, zangle));
             }
             else
             {
-                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                float zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
                 Quaternion desiardrotation = Quaternion.Euler(0, 0, zangle);
-                Instantiate(m15, transform.position + offset, Quaternion.Euler(dir));
+                Instantiate(m15, transform.position + offset, Quaternion.Euler(0, 0, zangle));   
             }
         }
         
