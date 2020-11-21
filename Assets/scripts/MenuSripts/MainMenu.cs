425deff6b53dd9214223a43dd0bc6ce967cfaca2
Mukul-dhiman <b18076@students.iitmandi.ac.in>
2020-11-21 21:08:22 +0530

    completed

--- Assets/scripts/MenuSripts/MainMenu.cs
+++ Assets/scripts/MenuSripts/MainMenu.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private AudioSource click_audio;
+    private void Start()
+    {
+        click_audio = GetComponent<AudioSource>();
+    }
+    public void onclick_audio()
+    {
+        click_audio.Play();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
